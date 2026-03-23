876266aa494df19d91ac970ea9a95b2dd2a44234
casssiopea <purpleturtle998@gmail.com>
2026-03-23 19:11:57 +0100

    Style Card

--- app/src/main/java/com/example/shoppinglistorganizer/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistorganizer/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import android.R.attr.checked
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import com.example.shoppinglistorganizer.ui.theme.ShoppingLIstOrganizerTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,11 +46,30 @@ data class ShoppingItem(
 
 @Composable
 fun ShoppingItemCard(item: ShoppingItem){
-    Text(
-        text = item.name,
-        fontSize =  18.sp,
-    )
+
+
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp)
+            .background(MaterialTheme.colorScheme.surfaceDim,
+                MaterialTheme.shapes.medium
+            )
+
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(checked = item.isBought, onCheckedChange = {
+
+        })
+        Text(
+            text = item.name,
+            modifier = Modifier.weight(1f),
+            fontSize = 18.sp
+        )
+
+    }
 }
+
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
