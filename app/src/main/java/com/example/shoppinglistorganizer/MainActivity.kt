f35c990082b44b8af7c139648eb494c8519ae416
casssiopea <purpleturtle998@gmail.com>
2026-03-23 19:56:13 +0100

    State List

--- app/src/main/java/com/example/shoppinglistorganizer/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistorganizer/MainActivity.kt
@@ -16,10 +16,23 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.getValue
 import android.R.attr.checked
+import android.icu.text.ListFormatter
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.shoppinglistorganizer.ui.theme.ShoppingLIstOrganizerTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +42,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingLIstOrganizerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(modifier = Modifier.padding(innerPadding)) {
+                        Text("Hello,Android")
+
                 }
             }
         }
@@ -44,8 +56,29 @@ data class ShoppingItem(
     val isBought: Boolean = false
 )
 
+
+
+    class ShoppingListViewModel : ViewModel() {
+        val shoppingList = mutableStateListOf(
+            ShoppingItem("молоко"),
+            ShoppingItem("хліб"),
+            ShoppingItem("масло"),
+            ShoppingItem("олія"),
+        )
+
+        fun toggleBought(index: Int) {
+            shoppingList[index] = shoppingList[index].copy(
+                isBought = !shoppingList[index].isBought
+            )
+        }
+    }
+
+
 @Composable
-fun ShoppingItemCard(item: ShoppingItem){
+fun ShoppingItemCard(
+    item: ShoppingItem,
+    onToggleBought : () -> Unit = {}
+){
 
 
     Row(
@@ -54,12 +87,12 @@ fun ShoppingItemCard(item: ShoppingItem){
             .background(MaterialTheme.colorScheme.surfaceDim,
                 MaterialTheme.shapes.medium
             )
-
+            .clickable{onToggleBought() }
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Checkbox(checked = item.isBought, onCheckedChange = {
-
+            onToggleBought()
         })
         Text(
             text = item.name,
@@ -70,29 +103,46 @@ fun ShoppingItemCard(item: ShoppingItem){
     }
 }
 
-
-@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun ShoppingItemCardPreview(){
-    ShoppingItemCard(ShoppingItem("молоко"))
+fun ShoppingListScreen(viewModel: ShoppingListViewModel= viewModel()) {
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp)
+    ) {
+        itemsIndexed(viewModel.shoppingList) { ix, item ->
+            ShoppingItemCard(item){
+                viewModel.toggleBought(ix)
+            }
+
+    }
+}
 }
 
 
 
+    @Preview
+    @Composable
+    fun ShoppingListScreenPreview() {
+            ShoppingListScreen()
+
+    }
 
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ShoppingLIstOrganizerTheme {
-        Greeting("Android")
-    }
-}
+fun ShoppingItemCardPreview() {
+        var toggleState by remember { mutableStateOf(false) }
+        ShoppingItemCard(
+            item = ShoppingItem("Молоко", isBought = toggleState)
+        ) {
+            toggleState = !toggleState
+        }
+    }}
+
+
+
+
+
+
